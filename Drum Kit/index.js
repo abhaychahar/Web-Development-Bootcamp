14265112d1d21acf3759f60236e758cbe344f48e
abhaychahar <abhaychahar.1602@gmail.com>
2023-09-07 21:53:53 +0530

    Drum Kit (With Button Animation)

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,74 +1,55 @@
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kickBass=new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        case "s":
+            var snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break; 
+        case "j":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+    }
+}
+
+function buttonAnimation(key) {
+    var activeButton=document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
 for(var i=0; i<document.querySelectorAll(".drum").length; i++) {
     var element=document.querySelectorAll(".drum")[i];
     element.addEventListener("click", function () {
         var buttonName=this.innerHTML;
-
-        switch (buttonName) {
-            case "w":
-                var audio=new Audio("sounds/crash.mp3");
-                audio.play();
-                break;
-            case "a":
-                var audio=new Audio("sounds/kick-bass.mp3");
-                audio.play();
-                break;
-            case "s":
-                var audio=new Audio("sounds/snare.mp3");
-                audio.play();
-                break;
-            case "d":
-                var audio=new Audio("sounds/tom-1.mp3");
-                audio.play();
-                break; 
-            case "j":
-                var audio=new Audio("sounds/tom-2.mp3");
-                audio.play();
-                break;
-            case "k":
-                var audio=new Audio("sounds/tom-3.mp3");
-                audio.play();
-                break;
-            case "l":
-                var audio=new Audio("sounds/tom-4.mp3");
-                audio.play();
-        }
+        makeSound(buttonName);
+        buttonAnimation(buttonName);
     });
 }
 
-function makeSoundByKeyboard(key) {
-    switch (key) {
-        case "w":
-            var audio=new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-        case "a":
-            var audio=new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-        case "s":
-            var audio=new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
-        case "d":
-            var audio=new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break; 
-        case "j":
-            var audio=new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-        case "k":
-            var audio=new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-        case "l":
-            var audio=new Audio("sounds/tom-4.mp3");
-            audio.play();
-    }
-}
-
 document.addEventListener("keypress", function (event) {
-    makeSoundByKeyboard(event.key);
+    makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 
