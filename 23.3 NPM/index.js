003b8d09733e53fb09fb6fe01be507d3ae7424a3
abhaychahar <abhaychahar.1602@gmail.com>
2023-10-08 00:27:39 +0530

    NPM

---
+++ 23.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+
+var silly=generateName();
+console.log(`My name is ${silly}.`);
+
+import superheroes from "superheroes";
+
+var superhero=superheroes.random();
+console.log(`I am ${superhero}`);
